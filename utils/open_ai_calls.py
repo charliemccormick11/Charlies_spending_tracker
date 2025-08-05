3172502d873753535b3956cb47ec8987b5b63059
Mackdacoder <charliemccormick2000@gmail.com>
2025-08-05 18:02:22 -0500

    Update open_ai_calls.py

--- utils/open_ai_calls.py
+++ utils/open_ai_calls.py
@@ -56,6 +56,7 @@ def open_ai_headers(uploaded_credit, credit_card, client):
     try:
     # Parse the JSON response
         st.session_state.column_info = json.loads(completion.choices[0].message.content)
+        st.write(st.session_state.column_info)
 
         
     except Exception as e:
