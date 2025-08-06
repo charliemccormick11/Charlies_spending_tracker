960ab12613de021e607c50644a926a8e0ef9bf06
Mackdacoder <charliemccormick2000@gmail.com>
2025-08-05 21:03:04 -0500

    Update open_ai_calls.py

--- utils/open_ai_calls.py
+++ utils/open_ai_calls.py
@@ -99,7 +99,7 @@ def open_ai_headers(uploaded_credit, credit_card, client):
             
             return total_credit_df
     except:
-        st.error("Please ensure you select "Other" on the sidebar for credit card if you don't use Chase!")
+        st.error("Please ensure you select 'Other' on the sidebar for credit card if you don't use Chase!")
     
 def open_ai_random_categorization(client):
 
