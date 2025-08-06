9f26b22c1d8ee22589482b8242a39c76b8fd3739
Mackdacoder <charliemccormick2000@gmail.com>
2025-08-05 21:29:12 -0500

    Update open_ai_calls.py

--- utils/open_ai_calls.py
+++ utils/open_ai_calls.py
@@ -24,15 +24,14 @@ def open_ai_headers(uploaded_credit, credit_card, client):
     1.2 If the first row contains data (e.g., values like "2023-01-01", "100"), set 'header' to False.
     2. Regardless of if there's a header, provide the column indices (starting from 0) for the following columns:
     - Transaction Date
-    - Description (this is the names of merchants, example is DUNKIN) 
+    - Description (example is DUNKIN) 
     - Amount
     - Category (Food & Drink, Groceries, Entertainment, Shopping, etc.) index is likely 3
     4. If there are two amount columns (Credit and Debit), provide their indices and include them as "credit" and "debit".
     4.4 If there is one amount column, put it as 'debit'.
     5. Return ONLY the response as a Python JSON dictionary with the following keys: 'header', 'transaction_date', 'description', 'credit', 'debit', 'category'. No additional text, explanations, or strings.
     6. Remove anything else but the dictionary
-    7. Ensure the "Description" index is assigned to a column with values that are in all caps. This will never inlude "Dining", "Merchandise", or other generic values
-    8. Nothing can be null
+    7. Nothing can be null
     """
 
     # Additional check before sending the prompt
@@ -41,7 +40,7 @@ def open_ai_headers(uploaded_credit, credit_card, client):
         prompt = prompt.replace("Please:", "Please that the category column index is provided as this is a Chase credit card!")
 
     else:
-        prompt = prompt.replace("- Category (Food & Drink, Groceries, Entertainment, Shopping, etc.) index is likely 3", "ignore any columns that include Dining, Merchandise, or other generic categories")
+        prompt = prompt.replace("- Category (Food & Drink, Groceries, Entertainment, Shopping, etc.) index is likely 3", "")
         prompt = prompt.replace(", 'category'.", ".")
 
 
