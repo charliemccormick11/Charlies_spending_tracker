f141ff349995db67900a67ace1862c1084cc251c
Mackdacoder <charliemccormick2000@gmail.com>
2025-08-05 22:01:46 -0500

    Update open_ai_calls.py

--- utils/open_ai_calls.py
+++ utils/open_ai_calls.py
@@ -59,7 +59,7 @@ def open_ai_headers(uploaded_credit, credit_card, client):
     # Parse the JSON response
         st.session_state.column_info = json.loads(completion.choices[0].message.content)
         if st.session_state.column_info['Description'] == None:
-            st.session_state.column_info['Description']= credit_sample.columns.tolist().index('Description')
+            st.session_state.column_info['Description']= 4
         
 
         
