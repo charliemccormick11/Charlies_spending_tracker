f230cb988b07c773b1a3ceb26b985d22c81b5b57
Mackdacoder <charliemccormick2000@gmail.com>
2025-08-10 13:48:20 -0500

    Update csv_download.py

--- utils/csv_download.py
+++ utils/csv_download.py
@@ -51,3 +51,4 @@ def download_as_csv():
 
     #code setting up future categories
     st.session_state.future_categories = st.session_state.spend_df[["Description", "Category"]].drop_duplicates(subset=["Description"])
+    st.session_state.future_categories["Financial Type"] = "Future Category"
